1525cd040f08908f2c118fbce3f4cd494af570d8
lvcyklm02 <87832460+lvcyklm02@users.noreply.github.com>
2024-07-18 23:47:09 -0400

    tailwind and pages

--- wiggle/wilg_website/urls.py
+++ wiggle/wilg_website/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
+    path("alumnae/", views.alumnae, name="alumnae"),
+    path("summer/", views.summer, name="summer"),
+    path("rush/", views.rush, name="rush"),
+    path("login/", views.login, name="login"),
+    path("house/", views.house, name="house"),
 ]
--- wiggle/wilg_website/views.py
+++ wiggle/wilg_website/views.py
@@ -8,6 +8,20 @@ from django.http import HttpResponse
 def home(request):
     return render(request, "pages/home.html")
 
-
 def about(request):
     return render(request, "pages/about.html")
+
+def house(request):
+    return render(request, "pages/house.html")
+
+def rush(request):
+    return render(request, "pages/rush.html")
+
+def alumnae(request):
+    return render(request, "pages/alumnae.html")
+
+def summer(request):
+    return render(request, "pages/summer.html")
+
+def login(request):
+    return render(request, "pages/login.html")
